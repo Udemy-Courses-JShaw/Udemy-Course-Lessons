fae044d96d0d97d64f6f871a53e5e10b649cfc94
Jarrod G Shaw <Jarrod.g.shaw@gmail.com>
2018-10-05 15:20:58 -0700

    Expanded the OpenDoor.cpp
    Expanded OpenDoor.cpp to make the doors open using the pressurePlate trigger Vol.
    
    Other changed:
     -.gitIngore updated
     -Pulled contend used to a folder to reduce errors for other Users
       - Re-associated said content
       - Created a Pink and Green Chair Material (eventally will have weight diffrerences
     - Created Grabber.h + .cpp; No changes
    
    NOTE - Had a devastating error trying to compile after incorrectly creating a second C++ Gamemode and trying to remove it. Had to revert the project from a previous commit.
    
    Project will not compile and run correctly.

--- Learn_C++/Section03/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ Learn_C++/Section03/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,11 +1,13 @@
 // Copyright Jarrod Shaw & Metal Muffin Ent. 2018
 
-#include "OpenDoor.h"
+#include "Grabber.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor() 
+UGrabber::UGrabber() 
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,33 +18,24 @@ UOpenDoor::UOpenDoor()
 
 
 // Called when the game starts
-void UOpenDoor::BeginPlay() 
+void UGrabber::BeginPlay() 
 {
 	Super::BeginPlay();
 
-	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
-}
-
-void UOpenDoor::OpenDoor() 
-{
-	Owner->SetActorRotation(FRotator(0.0f, DoorOpenAngle, 0.0f));
-}
-
-void UOpenDoor::CloseDoor() 
-{
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting In!!"));
 
 }
 
 
 // Called every frame
-void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) 
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) 
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
-	{
-		OpenDoor();
-	}
+	// Get Player Vew point this/every tick
+	//GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(); //TODO Finish this
+	// Ray-cast out to reach distance
 
+	// see what we hit
 }
 
